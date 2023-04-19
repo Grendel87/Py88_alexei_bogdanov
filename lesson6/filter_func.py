e1de8f4e5c1b8abd924c16e51d9a0ff2c25835a9
alexei <3914661v@gmail.com>
2023-04-19 12:27:23 +0300

    hotfix

--- lesson6/filter_func.py
+++ lesson6/filter_func.py
@@ -1,7 +1,9 @@
 names = ("Alexei", "Bil", "Carl", "Fiona")
 
-def filter_tuple(tpl, greater_4 = True):
+
+def filter_tuple(tpl, greater_4=True):
     return tuple(filter(lambda x: 4 < len(x) if greater_4 else len(x) <= 3, tpl))
 
+
 print(filter_tuple(names))
-print(filter_tuple(names, greater_4 = False))
+print(filter_tuple(names, greater_4=False))
