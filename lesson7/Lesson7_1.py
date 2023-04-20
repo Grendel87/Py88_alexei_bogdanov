2ff7ac4bf129ae504d97b53a14ec999f4c6c84dc
alexei <3914661v@gmail.com>
2023-04-20 20:13:48 +0300

    completed lesson7 part 1

--- lesson7/Lesson7_1.py
+++ lesson7/Lesson7_1.py
@@ -4,8 +4,13 @@ str3 = input()
 str4 = input()
 
 with open("example.txt", "w") as f:
-    f.write("str1\nstr2")
+    f.write(str1)
+    f.write("\n")
+    f.write(str2)
 
 f = open("example.txt", "a")
-f.write("\nstr3\nstr4")
+f.write("\n")
+f.write(str3)
+f.write("\n")
+f.write(str4)
 f.close
